ad6d8795ab378772b0ddd1bb146357d79eb9a8d5
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2017-11-02 11:28:43 +0100

    picker, stats. basically almost done

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,27 @@
 var gameStatus = "notStarted";
 var newGame = document.getElementById("js-newGameElement");
+var gamePicker = document.getElementById("js-playerPickElement");
+var gameStats = document.getElementById("js-resultsTableElement");
 
 function setGameElements() {
 	
 	switch(gameStatus) {
 		case "started":
 			newGame.style.display = "none";
+			gamePicker.style.display = "block";
+			gameStats.style.display = "block";
 			break;
 		case "ended":
 			break;
 		case "notStarted":
+			newGame.style.display = "block";
+			gamePicker.style.display = "none";
+			gameStats.style.display = "none";
 			break;
 		default:
 			newGame.style.display = "block";
+			gamePicker.style.display = "none";
+			gameStats.style.display = "none";
 		
 	}
 }
