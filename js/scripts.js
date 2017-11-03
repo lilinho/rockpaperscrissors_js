94fc4a495d71a1ecd5155e76c632e20c62852c3c
Tomasz Pawelec <tpawelec@outlook.com>
2017-11-03 11:27:50 +0100

    'animation ' for computers pick

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,33 @@ var gameStatus = "notStarted";
 var newGame = document.getElementById("js-newGameElement");
 var gamePicker = document.getElementById("js-playerPickElement");
 var gameStats = document.getElementById("js-resultsTableElement");
+var picksRock = document.getElementById("js-playerPick_rock");
+var picksPaper = document.getElementById("js-playerPick_paper");
+var picksScissors = document.getElementById("js-playerPick_scissors");
+var picksLizard = document.getElementById("js-playerPick_lizard");
+var picksSpock = document.getElementById("js-playerPick_spock");
 
+var computerButtons = document.getElementsByClassName("c-btns");
+var previousIndex = 0;
+var interval;
+function showButton() {
+	console.log("a");
+	computerButtons[previousIndex].style.display = "none";
+	previousIndex = Math.floor(Math.random()*5);
+	computerButtons[previousIndex].style.display = "block";
+	
+}
+
+function eventForClick() {
+	
+	interval = setInterval(function(){ 
+		console.log("a");
+		computerButtons[previousIndex].style.display = "none";
+		previousIndex = Math.floor(Math.random()*5);
+		computerButtons[previousIndex].style.display = "block";	}, 100);
+	setTimeout(function () {clearInterval(interval);}, 2000);
+	
+}
 function setGameElements() {
 	
 	switch(gameStatus) {
@@ -34,3 +60,9 @@ newGame.addEventListener("click", function() {
 	gameStatus = "started";
 	setGameElements();
 });
+
+picksRock.addEventListener("click", function() {eventForClick();});
+picksScissors.addEventListener("click", function() {eventForClick();});
+picksPaper.addEventListener("click", function() {eventForClick();});
+picksLizard.addEventListener("click", function() {eventForClick();});
+picksSpock.addEventListener("click", function() {eventForClick();});
